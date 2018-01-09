0fac56deace94a57a0cdf9cf011b947dd115e056
Deesthortered <anatoly.davydko@gmail.com>
2018-01-09 16:49:38 +0200

    start

---
+++ HuffmanArchiver/BitSet.h
@@ -0,0 +1,42 @@
+#pragma once
+
+namespace spaceBitSet
+{
+	class BitSet
+	{
+		char *arr;
+		size_t bit_size();
+		size_t real_size();
+
+	public:
+		BitSet()
+		{
+
+		}
+		~BitSet()
+		{
+
+		}
+
+		bool operator[] (size_t iter)
+		{
+
+		}
+		void Reserve(size_t cnt)
+		{
+
+		}
+		void ShrinkToFit()
+		{
+
+		}
+
+		bool GetValue(size_t iter)
+		{
+			
+		}
+
+	private:
+
+	};
+}

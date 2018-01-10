7c3f42611dfd562e648b146ec4e1c73daadea507
Deesthortered <anatoly.davydko@gmail.com>
2018-01-10 09:56:43 +0200

    BitSet is almost created

--- HuffmanArchiver/BitSet.h
+++ HuffmanArchiver/BitSet.h
@@ -15,6 +15,13 @@ namespace spaceBitSet
 			this->bit_size = 0;
 			this->byte_size = 0;
 		}
+		BitSet(size_t bit_sz)
+		{
+			size_t byte_sz = bit_sz / 8 + (bit_sz % 8 ? 1 : 0);
+			this->arr = new char[byte_size];
+			this->byte_size = byte_sz;
+			this->bit_size = bit_sz;
+		}
 		~BitSet()
 		{
 			delete[] this->arr;
@@ -24,6 +31,11 @@ namespace spaceBitSet
 
 		void Resize(size_t bit_sz)
 		{
+			if (!bit_sz)
+			{
+				this->~BitSet();
+				return;
+			}
 			size_t byte_sz = bit_sz / 8 + (bit_sz % 8 ? 1 : 0);
 			char *new_arr = new char[byte_sz];
 			for (size_t i = 0; i < this->byte_size; i++)
@@ -35,6 +47,7 @@ namespace spaceBitSet
 		}
 		void Reserve(size_t bit_sz)
 		{
+			if (bit_sz < this->bit_size) return;
 			size_t byte_sz = bit_sz / 8 + (bit_sz % 8 ? 1 : 0);
 			char *new_arr = new char[byte_sz];
 			for (size_t i = 0; i < this->byte_size; i++)
@@ -45,7 +58,22 @@ namespace spaceBitSet
 		}
 		void ShrinkToFit()
 		{
+			size_t byte_sz = this->bit_size / 8 + (this->bit_size % 8 ? 1 : 0);
+			char *new_arr = new char[byte_sz];
+			for (size_t i = 0; i < byte_sz; i++)
+				new_arr[i] = this->arr[i];
+			delete[] this->arr;
+			this->arr = new_arr;
+			this->byte_size = byte_sz;
+		}
 
+		size_t BitSize()
+		{
+			return this->bit_size;
+		}
+		size_t ByteSize()
+		{
+			return this->byte_size;
 		}
 
 		bool GetValue(size_t iter)
@@ -75,10 +103,9 @@ namespace spaceBitSet
 			}
 		}
 
-		
-		const bool operator< (const BitSet &a, const BitSet &b) 
+		friend bool operator< (BitSet &a, BitSet &b) 
 		{
-
+			
 		}
 
 	};
